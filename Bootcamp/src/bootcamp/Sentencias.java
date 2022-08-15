33ed3bf91dc523dc54ef82dc67de8e2f172f60f8
Germán Alonso <99427438+alonsoger@users.noreply.github.com>
2022-08-15 10:51:57 -0300

    Add files via upload

---
+++ Bootcamp/src/bootcamp/Sentencias.java
@@ -0,0 +1,64 @@
+package bootcamp;
+
+public class Sentencias {
+     
+    public static void main(String[] args) {
+        /* -------------- BUCLE IF -----------------
+        int numeroIf = 10;
+        if(numeroIf < 0){
+            System.out.print("Numero negativo");
+        } else if (numeroIf > 0) {
+            System.out.print("Numero positivo");
+        } else {
+            System.out.print("El numero es 0");
+        }
+        */
+        
+        /* -------------- BUCLE WHILE -----------------
+        int numeroWhile = 0;
+        while(numeroWhile < 3){
+            System.out.print(numeroWhile);
+            numeroWhile++;
+        }
+        */
+        
+        /*-------------- BUCLE DO WHILE -----------------
+        int numeroWhile = 0;
+        do {
+            System.out.print(numeroWhile);
+            numeroWhile++;
+        } while(numeroWhile < 3);
+        */
+        
+        /*-------------- BUCLE FOR -----------------
+        
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.print(numeroFor);
+        }
+        
+        */
+        
+        /*-------------- BUCLE SWITCH -----------------
+        
+        var estacion = "Inverno";
+        switch (estacion) {
+            case "Verano":
+                System.out.println("Es verano");
+            case "Otoño":
+                System.out.println("Es otoño");
+            case "Primavera":
+                System.out.println("Es primavera");
+            case "Inverno":
+                System.out.println("Es invierno");
+            default:
+                System.out.println("No estamos hablando de una estacion");
+                    
+        }
+        */
+        
+        
+        
+    }
+     
+    
+}
