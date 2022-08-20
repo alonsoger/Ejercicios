803965edc3c50267a6655ab9b303809a6d729ae5
Germán Alonso <99427438+alonsoger@users.noreply.github.com>
2022-08-20 09:53:45 -0300

    Add files via upload

---
+++ Bootcamp/Proyectonode/elementos.js
@@ -0,0 +1,12 @@
+let nombre = "German";
+let edad = 26;
+let desarrollador = true;
+
+const fecha_nacimiento = new Date(1996, 7, 29);
+
+const libro = {
+    titulo: "Universos paralelos",
+    autor: "Michio Kaku",
+    fecha: "2008",
+    url: "Google"
+}
