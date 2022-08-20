4a26f9508ebc7afac2cf7b6bdf91d07b52a817ff
German Alonso <germii29@hotmail.com>
2022-08-20 18:29:24 -0300

    Estructuras de control

---
+++ Bootcamp/Proyectonode/Estructurascontrol/factorial-break.js
@@ -0,0 +1,10 @@
+
+let factorial = 1;
+let indice = 1;
+while (indice < 100) {  
+    if (indice > 10){
+        break;
+    }
+    console.log(factorial = factorial*indice);
+    indice++;
+}
---
+++ Bootcamp/Proyectonode/Estructurascontrol/factorial-while.js
@@ -0,0 +1,6 @@
+let factorial = 1;
+let indice = 1;
+while (indice <= 10){ 
+    console.log(factorial = factorial*indice);
+    indice++;
+}
