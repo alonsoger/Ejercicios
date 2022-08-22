1fcbc67f22c0ce56d283fe80237f9e3d4546003c
German Alonso <germii29@hotmail.com>
2022-08-22 14:50:40 -0300

    Numeros

---
+++ Bootcamp/Proyectonode/numeros.js
@@ -0,0 +1,6 @@
+let alturaCm = 1740;
+let alturaM = 1.74;
+let pesoKg = 62.6;
+let alturaRed = alturaM.toFixed()
+let pesoRed = parseInt(pesoKg);
+let numer_max = ((Number.MAX_VALUE + 1 ) === Number.MAX_VALUE);
