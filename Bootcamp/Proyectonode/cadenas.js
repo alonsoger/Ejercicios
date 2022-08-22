b9e6bd651879158c626b29eb75bfece06e56307f
German Alonso <germii29@hotmail.com>
2022-08-22 10:48:31 -0300

    Cadenas

---
+++ Bootcamp/Proyectonode/cadenas.js
@@ -0,0 +1,15 @@
+let nombre = "German";
+let apellido = "Alonso";
+let estudiante = `${nombre} ${apellido}`;
+let estudiante2 = nombre + " " + apellido;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let numCadena = estudiante.length;
+let firstLetter = nombre[0]
+
+let numero = apellido.length;
+let lastLetter = apellido[(numero-1)];
+
+let espacioCero = estudiante.trim().length;
+
+let contenido = estudiante.includes("German");
